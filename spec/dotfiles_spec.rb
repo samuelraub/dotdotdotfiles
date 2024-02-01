1b24fc0eba59ce8902c80426818f4002316db5e5
Samuel Raub <samuel.raub@gmail.com>
2024-02-01 13:52:30 +0100

    init

---
+++ spec/dotfiles_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dotfiles do
+  it "has a version number" do
+    expect(Dotfiles::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
